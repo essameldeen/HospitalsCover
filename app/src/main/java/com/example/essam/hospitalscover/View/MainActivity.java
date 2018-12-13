845dd5dfd8a5482eafe2f107419acaecb2fe087d
Amr <amr_ashraf_momhamd_amin@yahoo.com>
2018-12-13 16:19:04 +0200

    splash screen v1

--- app/src/main/java/com/example/essam/hospitalscover/View/MainActivity.java
+++ app/src/main/java/com/example/essam/hospitalscover/View/MainActivity.java
@@ -1,53 +1,27 @@
 package com.example.essam.hospitalscover.View;
 
-import android.arch.lifecycle.Observer;
-import android.arch.lifecycle.ViewModelProvider;
-import android.arch.lifecycle.ViewModelProviders;
-import android.support.annotation.Nullable;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.example.essam.hospitalscover.Model.Example;
-import com.example.essam.hospitalscover.ModelView.TempModelView;
 import com.example.essam.hospitalscover.R;
 
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    TempModelView tempModelView;
-    TextView textView;
+
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.test);
-        tempModelView = ViewModelProviders.of(this).get(TempModelView.class);
+        new Handler().postDelayed(() -> {
+            Intent mainIntent = new Intent(MainActivity.this, HomePage.class);
+            MainActivity.this.startActivity(mainIntent);
+            MainActivity.this.finish();
+        }, SPLASH_DISPLAY_LENGTH);
 
-        initListener();
-        getData();
     }
-
-    private void getData() {
-        tempModelView.getAllTest();
-    }
-
-    private void initListener() {
-        tempModelView.getTest().observe(this, new Observer<Example>() {
-            @Override
-            public void onChanged(@Nullable Example strings) {
-                if (strings != null) {
-                    textView.setText(strings.getData()+" " +strings.getTime());
-
-                } else {
-                    Toast.makeText(MainActivity.this, "Null", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
-
 
 }
