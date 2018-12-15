6608c1d29abf495b710168fb31dd52f60e3293f4
Amr <amr_ashraf_momhamd_amin@yahoo.com>
2018-12-15 09:53:25 +0200

    add notification

--- app/src/main/java/com/example/essam/hospitalscover/FireBase/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/essam/hospitalscover/FireBase/MyFirebaseMessagingService.java
@@ -1,14 +1,104 @@
 package com.example.essam.hospitalscover.FireBase;
 
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 
+import com.example.essam.hospitalscover.View.HomePage;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+        if (remoteMessage.getData().isEmpty()) {
+            sendNotifcation(remoteMessage);
+        } else {
+            sendNotifcation(remoteMessage.getData());
+        }
+    }
+
+    private void sendNotifcation(Map<String, String> data) {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "HospitalsCover";
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            @SuppressLint("WrongConstant") NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "ESSAM Notification"
+                    , NotificationManager.IMPORTANCE_MAX);
+
+            notificationChannel.setDescription("New Reservation");
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableLights(true);
+            notificationChannel.enableVibration(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+
+        }
+        // Intent intent = new Intent("ESSAMMOHAMED.HOSPITALCOVER");
+        Intent intent = new Intent(getApplication(),HomePage.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        builder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(android.support.v4.R.drawable.notification_icon_background)
+                .setTicker("TRAKER")
+                .setContentIntent(pendingIntent)
+                .setContentTitle("New Reservation")
+                .setContentText("Pleas Find Your New Reservation")
+                .setContentInfo("info");
+        notificationManager.notify(1, builder.build());
+
+    }
+
+    private void sendNotifcation(RemoteMessage remoteMessage) {
+        Map<String, String> data = remoteMessage.getData();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "HospitalsCover";
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            @SuppressLint("WrongConstant") NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "ESSAM Notification"
+                    , NotificationManager.IMPORTANCE_MAX);
+
+            notificationChannel.setDescription("Notification Demo App For Test");
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableLights(true);
+            notificationChannel.enableVibration(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+
+        }
+        Intent intent = new Intent(getApplication(),HomePage.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        builder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(android.support.v4.R.drawable.notification_icon_background)
+                .setTicker("TRAKER")
+                .setContentIntent(pendingIntent)
+                .setContentTitle("New Reservation")
+                .setContentText("Pleas Find Your New Reservation")
+                .setContentInfo("Please");
+
+        notificationManager.notify(1, builder.build());
+
+
     }
 }
--- app/src/main/java/com/example/essam/hospitalscover/View/MainActivity.java
+++ app/src/main/java/com/example/essam/hospitalscover/View/MainActivity.java
@@ -7,17 +7,23 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.essam.hospitalscover.ModelView.MyMACAdress;
 import com.example.essam.hospitalscover.R;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final int SPLASH_DISPLAY_LENGTH = 2000;
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
 
+    void subscribeToTopic(String mac) {
+        FirebaseMessaging.getInstance().subscribeToTopic(mac);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        subscribeToTopic(MyMACAdress.getMacAddr());
         new Handler().postDelayed(() -> {
             Intent mainIntent = new Intent(MainActivity.this, HomePage.class);
             MainActivity.this.startActivity(mainIntent);
@@ -25,7 +31,5 @@ public class MainActivity extends AppCompatActivity {
         }, SPLASH_DISPLAY_LENGTH);
 
     }
-
-
 
 }
